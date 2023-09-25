1e782a30cd0758c2ce8da13f663f5b81df855a54
thauanymielnik <134974959+thauanymielnik@users.noreply.github.com>
2023-09-25 20:05:55 -0300

    a

--- main.js
+++ main.js
@@ -15,4 +15,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom (idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
