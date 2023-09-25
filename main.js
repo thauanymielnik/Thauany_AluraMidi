1d0a3329b2e370cc25d41264eb4f76e1a99f6ada
thauanymielnik <134974959+thauanymielnik@users.noreply.github.com>
2023-09-25 19:33:12 -0300

    a

--- main.js
+++ main.js
@@ -1,53 +1,28 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento)
+
+    const idAudio
+
+    //'#som_{instrumento'
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#Toca_Som_Pom')
+    };
+
+    contador = contador + 1;
+
+    console.log(contador);
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-function tocaSomSplash () {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
